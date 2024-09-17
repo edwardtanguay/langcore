6cf449e588ef8c50b1b2245f2ad739c63ff8eccc
Edward Tanguay (windows) <edward@tanguay.info>
2024-09-17 10:18:46 +0200

    past

--- src/pages/PageDutchVerbs.tsx
+++ src/pages/PageDutchVerbs.tsx
@@ -14,6 +14,8 @@ export const PageDutchVerbs = () => {
 						<div className='ml-3 font-mono text-[.8rem] flex flex-col gap-2 bg-white p-3 rounded mb-4 mt-2'>
 							<p>vandaag <span className='present'>{dutchVerb.present}</span> ik</p>
 							<p>we <span className='present'>{dutchVerb.infinitive}</span> vandaag</p>
+							<p>gisteren <span className='past'>{dutchVerb.imperfectSingular}</span> ik</p>
+							<p>we <span className='past'>{dutchVerb.imperfectPlural}</span> gisteren</p>
 						</div>
 					</div>
 				)
