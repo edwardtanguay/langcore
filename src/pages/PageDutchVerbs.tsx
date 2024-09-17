06133b68659bc57a34f14a77d34618196c521066
Edward Tanguay (windows) <edward@tanguay.info>
2024-09-17 09:28:58 +0200

    showing words

--- src/pages/PageDutchVerbs.tsx
+++ src/pages/PageDutchVerbs.tsx
@@ -1,12 +1,16 @@
 import dutchVerbs from '../data/dutchVerbs.json';
 
+dutchVerbs.sort((a, b) => a.rank < b.rank ? 1 : -1);
+
+const devMode = false;
+
 export const PageDutchVerbs = () => {
 	return (
 		<>
 			{dutchVerbs.map(dutchVerb => {
 				return (
 					<div key={dutchVerb.dpodId}>
-						<p>{dutchVerb.english}</p>
+						<p>{dutchVerb.english}{devMode && <span> <sup className='text-[.8rem] text-gray-500'>{dutchVerb.rank}</sup></span>}</p>
 					</div>
 				)
 			})}
