533e88831662e25a64ffcf1be9dcd05cfec46466
Edward Tanguay (windows) <edward@tanguay.info>
2024-09-22 22:19:14 +0200

    getting letters

--- src/pages/PageProfile.tsx
+++ src/pages/PageProfile.tsx
@@ -1,13 +1,19 @@
+import React from "react";
 import { useStoreState } from "../store/hooks.ts"
 
 export const PageProfile = () => {
 	const { firstName } = useStoreState((state) => state.profileModel);
 
+	const handleChangeFirstName = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const value = e.target.value;
+		console.log(111, value);
+	}
+
 	return (
 		<form>
 			<div className="formRow">
 				<label htmlFor="firstName">First Name:</label>
-				<input type="text" id="fullName" value={firstName}></input>
+				<input type="text" id="firstName" onChange={e => handleChangeFirstName(e)} value={firstName}></input>
 			</div>
 		</form>
 	)
