011f7079e2714e4ea4285971adf9de61047e394d
Edward Tanguay (windows) <edward@tanguay.info>
2024-09-22 23:37:50 +0200

    changeing state

--- src/components/ManageArea.tsx
+++ src/components/ManageArea.tsx
@@ -1,4 +1,4 @@
-import { useStoreState } from "../store/hooks.ts"
+import { useStoreActions, useStoreState } from "../store/hooks.ts"
 import { DutchVerb } from "../types.ts";
 
 interface IProps {
@@ -7,15 +7,16 @@ interface IProps {
 
 export const ManageArea = ({ dutchVerb }: IProps) => {
 	const { learnedVerbs } = useStoreState((state) => state.profileModel);
+	const { addVerbLearned, removeVerbLearned } = useStoreActions((actions) => actions.profileModel);
 
 	const learned = learnedVerbs.includes(dutchVerb.dpodId);
 
 	return (
 		<div>
 			{learned ? (
-				<button className="buttonManageArea buttonLearned">Learned</button>
+				<button onClick={() => removeVerbLearned(dutchVerb.dpodId)} className="buttonManageArea buttonLearned">Learned</button>
 			) : (
-				<button className="buttonManageArea buttonNotLearned">Not Learned</button>
+				<button onClick={() => addVerbLearned(dutchVerb.dpodId)} className="buttonManageArea buttonNotLearned">Not Learned</button>
 			)}
 		</div>
 	)
--- src/store/models/profileModel.ts
+++ src/store/models/profileModel.ts
@@ -7,12 +7,22 @@ export interface ProfileModel {
 
 	// actions
 	setFirstName: Action<this, string>;
+	addVerbLearned: Action<this, string>;
+	removeVerbLearned: Action<this, string>;
 }
 
 export const profileModel: ProfileModel = {
-	firstName: '',
-	learnedVerbs: ['QkrYyM', 'dTX5Nf', 'Gp1pht'],
+	firstName: "",
+	learnedVerbs: [],
 	setFirstName: action((state, firstName) => {
-		state.firstName = firstName; 
-	})
+		state.firstName = firstName;
+	}),
+	addVerbLearned: action((state, verbIdCode) => {
+		console.log('ADD', verbIdCode);
+		state.learnedVerbs.push(verbIdCode);
+	}),
+	removeVerbLearned: action((state, verbIdCode) => {
+		console.log('REMOVE', verbIdCode);
+		state.learnedVerbs = state.learnedVerbs.filter((m) => m !== verbIdCode);
+	}),
 };
