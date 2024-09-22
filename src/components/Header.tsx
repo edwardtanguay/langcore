74f3a06b5715c10414de0892e82e5c91deb9c62f
Edward Tanguay (windows) <edward@tanguay.info>
2024-09-22 22:13:40 +0200

    testing on top

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,10 @@ import { Nav } from "./Nav";
 export const Header = () => {
 	return (
 		<>
+			<div className="flex items-center justify-between">
 			<h1 className="text-3xl mb-3 text-slate-800">Language Core</h1>
+			<p>TESTING</p>
+			</div>
 			<Nav/>
 		</>
 	);
