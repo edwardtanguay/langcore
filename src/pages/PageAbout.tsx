d529e09f72d3355596020b6f75dd08e373c2fb89
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-01 20:48:48 +0200

    test

--- src/pages/PageAbout.tsx
+++ src/pages/PageAbout.tsx
@@ -1,5 +1,18 @@
 export const PageAbout = () => {
+
+	const online = false;
+
 	return (
-		<p>I use this site to learn languages. See <a className="underline" target="_blank" href="https://tanguay-eu.vercel.app">more of my projects</a>.</p>
+		<>
+			<p>I use this site to learn languages. See <a className="underline" target="_blank" href="https://tanguay-eu.vercel.app">more of my projects</a>.</p>
+
+			{
+				online ? (
+					<p>I am online.</p>
+				) : (
+					<p>not online</p>
+				)
+			}
+		</>
 	)
 }
