d333409748b49453eb4d8d35976f46cf78033902
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-01 21:11:29 +0200

    about fixed

--- src/pages/PageAbout.tsx
+++ src/pages/PageAbout.tsx
@@ -1,18 +1,7 @@
 export const PageAbout = () => {
-
-	const online = false;
-
 	return (
 		<>
 			<p>I use this site to learn languages. See <a className="underline" target="_blank" href="https://tanguay-eu.vercel.app">more of my projects</a>.</p>
-
-			{
-				online ? (
-					<p>I am online.</p>
-				) : (
-					<p>not online</p>
-				)
-			}
 		</>
 	)
 }
