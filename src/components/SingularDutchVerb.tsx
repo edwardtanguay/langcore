7db6b752a083d96b67bcc0408e212bbba7ec1912
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-03 00:03:21 +0200

    refactored to SingularDutchVerb component

---
+++ src/components/SingularDutchVerb.tsx
@@ -0,0 +1,63 @@
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import { DevArea } from "./DevArea";
+import * as config from '../config';
+import { ManageArea } from "./ManageArea";
+import { useContext } from "react";
+import { AppContext } from "../AppContext";
+import { DutchVerb, DutchVerbExample } from "../types";
+
+interface IProps {
+	dutchVerb: DutchVerb;
+	learnedVerbs: string[];
+}
+
+export const SingularDutchVerb = ({ dutchVerb, learnedVerbs }: IProps) => {
+	const { handleIsOpenToggle } = useContext(AppContext);
+	const { dutchVerbs, setDutchVerbs } = useContext(AppContext);
+
+	const isLearned = learnedVerbs.includes(dutchVerb.dpodId);
+
+	const handleExampleToggle = (example: DutchVerbExample) => {
+		example.isOpen = !example.isOpen;
+		const _dutchVerbs = structuredClone(dutchVerbs);
+		setDutchVerbs(_dutchVerbs);
+	}
+
+
+	return (
+		<div key={dutchVerb.dpodId} >
+			<p onClick={() => handleIsOpenToggle(dutchVerb)} className={`bg-slate-300 mb-3 px-2 py-1 cursor-pointer rounded ${isLearned ? 'verbLearned' : ''}`}><span className='font-semibold text-[1.4rem]'>{dutchVerb.english}{config.devMode() && <span> <sup className='text-[.8rem] text-gray-500'>Rank: {dutchVerb.rank}, <span style={{ color: dutchVerb.examples.length === 0 ? 'red' : '' }}>Examples: {dutchVerb.examples.length}</span></sup></span>}</span>
+				{dutchVerb.isOpen && (
+					<span> - {dutchVerb.infinitive}</span>
+				)}</p>
+			{
+				dutchVerb.isOpen && (
+					<div className='ml-3 font-mono text-[1rem] flex flex-col gap-4 bg-white p-3 rounded mb-4 mt-2'>
+						<div className="flex justify-between">
+							<p>vandaag <span className='present'><a href={dutchVerb.presentExampleLink} target="_blank">{dutchVerb.present}</a></span> ik</p>
+							<a href={dutchVerb.conjugationLink} target="_blank"><FaMagnifyingGlass /></a>
+						</div>
+						<p>vandaag <span className='present'><a href={dutchVerb.infinitiveExampleLink} target="_blank">{dutchVerb.infinitive}</a></span> we</p>
+						<p>gisteren <span className='past'><a href={dutchVerb.imperfectSingularExampleLink} target="_blank">{dutchVerb.imperfectSingular}</a></span> ik</p>
+						<p>gisteren <span className='past'><a href={dutchVerb.imperfectPluralExampleLink} target="_blank">{dutchVerb.imperfectPlural}</a></span> we</p>
+						<p>{dutchVerb.participleNoun} <span className={`font-bold ${dutchVerb.participleHelper === 'ben' ? 'text-red-600' : ''}`}>{dutchVerb.participleHelper}</span> gisteren <span className='participle'><a href={dutchVerb.participleExampleLink} target="_blank">{dutchVerb.participleVerb}</a></span></p>
+						<div className="text-sm -mt-3">
+							{dutchVerb.examples.map(example => {
+								return (
+									<div key={example.dpodId} className="mt-3">
+										<div onClick={() => handleExampleToggle(example)} className="border rounded-t-md py-1 px-2 bg-slate-200 italic text-slate-800 cursor-pointer border-r-slate-400 border-l-slate-400 border-t-slate-400">{example.english}</div>
+										{example.isOpen && (
+											<div className="border rounded-b-md py-1 px-2 bg-slate-300 font-bold border-r-slate-400 border-l-slate-400 border-b-slate-400">{example.dutch}</div>
+										)}
+									</div>
+								)
+							})}
+						</div>
+						<DevArea dutchVerb={dutchVerb} />
+						<ManageArea dutchVerb={dutchVerb} />
+					</div>
+				)
+			}
+		</div >
+	)
+}
--- src/pages/PageDutchVerbs.tsx
+++ src/pages/PageDutchVerbs.tsx
@@ -1,21 +1,11 @@
 import { useContext } from "react";
 import { AppContext } from "../AppContext";
-import { FaMagnifyingGlass } from "react-icons/fa6";
-import { DutchVerbExample } from "../types";
-import { DevArea } from "../components/DevArea";
-import * as config from '../config';
-import { ManageArea } from "../components/ManageArea";
 import { useStoreState } from "../store/hooks.ts"
+import { SingularDutchVerb } from "../components/SingularDutchVerb.tsx";
 
 export const PageDutchVerbs = () => {
-	const { dutchVerbs, setDutchVerbs, handleIsOpenToggle } = useContext(AppContext);
+	const { dutchVerbs } = useContext(AppContext);
 	const { learnedVerbs } = useStoreState((state) => state.profileModel);
-
-	const handleExampleToggle = (example: DutchVerbExample) => {
-		example.isOpen = !example.isOpen;
-		const _dutchVerbs = structuredClone(dutchVerbs);
-		setDutchVerbs(_dutchVerbs);
-	}
 
 	const percent = ((learnedVerbs.length / dutchVerbs.length) * 100).toFixed(0);
 
@@ -23,43 +13,7 @@ export const PageDutchVerbs = () => {
 		<>
 			<h2 className="mb-4 text-center">You have learned {learnedVerbs.length} of {dutchVerbs.length} verbs: <b>{percent}%</b></h2>
 			{dutchVerbs.map(dutchVerb => {
-				const learned = learnedVerbs.includes(dutchVerb.dpodId);
-				return (
-					<div key={dutchVerb.dpodId} >
-						<p onClick={() => handleIsOpenToggle(dutchVerb)} className={`bg-slate-300 mb-3 px-2 py-1 cursor-pointer rounded ${learned ? 'verbLearned' : ''}`}><span className='font-semibold text-[1.4rem]'>{dutchVerb.english}{config.devMode() && <span> <sup className='text-[.8rem] text-gray-500'>Rank: {dutchVerb.rank}, <span style={{ color: dutchVerb.examples.length === 0 ? 'red' : '' }}>Examples: {dutchVerb.examples.length}</span></sup></span>}</span>
-							{dutchVerb.isOpen && (
-								<span> - {dutchVerb.infinitive}</span>
-							)}</p>
-						{
-							dutchVerb.isOpen && (
-								<div className='ml-3 font-mono text-[1rem] flex flex-col gap-4 bg-white p-3 rounded mb-4 mt-2'>
-									<div className="flex justify-between">
-										<p>vandaag <span className='present'><a href={dutchVerb.presentExampleLink} target="_blank">{dutchVerb.present}</a></span> ik</p>
-										<a href={dutchVerb.conjugationLink} target="_blank"><FaMagnifyingGlass /></a>
-									</div>
-									<p>vandaag <span className='present'><a href={dutchVerb.infinitiveExampleLink} target="_blank">{dutchVerb.infinitive}</a></span> we</p>
-									<p>gisteren <span className='past'><a href={dutchVerb.imperfectSingularExampleLink} target="_blank">{dutchVerb.imperfectSingular}</a></span> ik</p>
-									<p>gisteren <span className='past'><a href={dutchVerb.imperfectPluralExampleLink} target="_blank">{dutchVerb.imperfectPlural}</a></span> we</p>
-									<p>{dutchVerb.participleNoun} <span className={`font-bold ${dutchVerb.participleHelper === 'ben' ? 'text-red-600' : ''}`}>{dutchVerb.participleHelper}</span> gisteren <span className='participle'><a href={dutchVerb.participleExampleLink} target="_blank">{dutchVerb.participleVerb}</a></span></p>
-									<div className="text-sm -mt-3">
-										{dutchVerb.examples.map(example => {
-											return (
-												<div key={example.dpodId} className="mt-3">
-													<div onClick={() => handleExampleToggle(example)} className="border rounded-t-md py-1 px-2 bg-slate-200 italic text-slate-800 cursor-pointer border-r-slate-400 border-l-slate-400 border-t-slate-400">{example.english}</div>
-													{example.isOpen && (
-														<div className="border rounded-b-md py-1 px-2 bg-slate-300 font-bold border-r-slate-400 border-l-slate-400 border-b-slate-400">{example.dutch}</div>
-													)}
-												</div>
-											)
-										})}
-									</div>
-									<DevArea dutchVerb={dutchVerb} />
-									<ManageArea dutchVerb={dutchVerb} />
-								</div>
-							)
-						}
-					</div >
-				)
+				return <SingularDutchVerb dutchVerb={dutchVerb} learnedVerbs={learnedVerbs}/>
 			})}
 		</>
 	)
