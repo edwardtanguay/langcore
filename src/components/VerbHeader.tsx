c9b1b57958ec44ff73c649cd17e47d20de23573c
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-03 01:20:22 +0200

    showing two shades

--- src/components/VerbHeader.tsx
+++ src/components/VerbHeader.tsx
@@ -14,10 +14,11 @@ export const VerbHeader = ({ dutchVerb, learnedVerbs }: IProps) => {
 	const { userVerbs } = useStoreState((state) => state.profileModel);
 
 	const isLearned = learnedVerbs.includes(dutchVerb.dpodId);
+	const userVerb = userVerbs.find(m => m.dpodId === dutchVerb.dpodId);
+	const timesOpened = userVerb ? userVerb.timesOpened : 0;
 
 	return (
 		<>
-		[userverbs: {userVerbs.length}]
 			<p onClick={() => handleIsOpenToggle(dutchVerb)} className={`bg-slate-300 flex justify-between mb-3 px-2 py-1 cursor-pointer rounded ${isLearned ? 'verbLearned' : ''}`}>
 				<div>
 					<span className='font-semibold text-[1.4rem]'>{dutchVerb.english}{config.devMode() && <span> <sup className='text-[.8rem] text-gray-500'>Rank: {dutchVerb.rank}, <span style={{ color: dutchVerb.examples.length === 0 ? 'red' : '' }}>Examples: {dutchVerb.examples.length}</span></sup></span>}</span>
@@ -25,8 +26,8 @@ export const VerbHeader = ({ dutchVerb, learnedVerbs }: IProps) => {
 						<span> - {dutchVerb.infinitive}</span>
 					)}
 				</div>
-				<div className="flex flex-col justify-center font-mono opacity-50">
-					99
+				<div className={`flex flex-col justify-center font-mono ${timesOpened === 0 ? `opacity-20` : `opacity-70 text-orange-800`}`}>
+					{timesOpened}
 				</div>
 			</p>
 		</>
