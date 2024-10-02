d57f35dcb76405d1cff877d17b84494504bced17
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-03 00:53:35 +0200

    showing number

--- src/components/VerbHeader.tsx
+++ src/components/VerbHeader.tsx
@@ -8,17 +8,24 @@ interface IProps {
 	learnedVerbs: string[];
 }
 
-export const VerbHeader = ({dutchVerb, learnedVerbs}: IProps) => {
+export const VerbHeader = ({ dutchVerb, learnedVerbs }: IProps) => {
 	const { handleIsOpenToggle } = useContext(AppContext);
 
 	const isLearned = learnedVerbs.includes(dutchVerb.dpodId);
 
 	return (
 		<>
-			<p onClick={() => handleIsOpenToggle(dutchVerb)} className={`bg-slate-300 mb-3 px-2 py-1 cursor-pointer rounded ${isLearned ? 'verbLearned' : ''}`}><span className='font-semibold text-[1.4rem]'>{dutchVerb.english}{config.devMode() && <span> <sup className='text-[.8rem] text-gray-500'>Rank: {dutchVerb.rank}, <span style={{ color: dutchVerb.examples.length === 0 ? 'red' : '' }}>Examples: {dutchVerb.examples.length}</span></sup></span>}</span>
-				{dutchVerb.isOpen && (
-					<span> - {dutchVerb.infinitive}</span>
-				)}</p>
+			<p onClick={() => handleIsOpenToggle(dutchVerb)} className={`bg-slate-300 flex justify-between mb-3 px-2 py-1 cursor-pointer rounded ${isLearned ? 'verbLearned' : ''}`}>
+				<div>
+					<span className='font-semibold text-[1.4rem]'>{dutchVerb.english}{config.devMode() && <span> <sup className='text-[.8rem] text-gray-500'>Rank: {dutchVerb.rank}, <span style={{ color: dutchVerb.examples.length === 0 ? 'red' : '' }}>Examples: {dutchVerb.examples.length}</span></sup></span>}</span>
+					{dutchVerb.isOpen && (
+						<span> - {dutchVerb.infinitive}</span>
+					)}
+				</div>
+				<div className="flex flex-col justify-center font-mono opacity-50">
+					99
+				</div>
+			</p>
 		</>
 	)
 }
