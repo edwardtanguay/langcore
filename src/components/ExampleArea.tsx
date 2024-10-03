93fd7e887be0e5b21d15597bd1419ebca481bdb2
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-04 00:09:47 +0200

    audio link in

--- src/components/ExampleArea.tsx
+++ src/components/ExampleArea.tsx
@@ -17,6 +17,10 @@ export const ExampleArea = ({ examples }: IProps) => {
 		setDutchVerbs(_dutchVerbs);
 	}
 
+	const handleGoToAudioSite = (example: DutchVerbExample) => {
+		window.open(`https://translate.google.com/saved?sl=nl&tl=en&text=${example.dutch}&op=translate`, '_blank');
+	}
+
 
 	return (
 		<div className="text-sm -mt-3">
@@ -25,7 +29,7 @@ export const ExampleArea = ({ examples }: IProps) => {
 					<div key={example.dpodId} className="mt-3">
 						<div onClick={() => handleExampleToggle(example)} className="border rounded-t-md py-1 px-2 bg-slate-200 italic text-slate-800 cursor-pointer border-r-slate-400 border-l-slate-400 border-t-slate-400">{example.english}</div>
 						{example.isOpen && (
-							<div className="border rounded-b-md py-1 px-2 bg-slate-300 font-bold border-r-slate-400 border-l-slate-400 border-b-slate-400">{example.dutch}<PiSpeakerHighFill className="inline-block ml-1" /></div>
+							<div className="border rounded-b-md py-1 px-2 bg-slate-300 font-bold border-r-slate-400 border-l-slate-400 border-b-slate-400">{example.dutch}<PiSpeakerHighFill onClick={() => handleGoToAudioSite(example)} className="cursor-pointer inline-block ml-1" /></div>
 						)}
 					</div>
 				)
