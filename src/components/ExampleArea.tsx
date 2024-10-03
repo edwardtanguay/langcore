7bda3e3dcd496dcd98d9a3555e8f7b468139a7b6
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-04 00:21:07 +0200

    mock toggle

--- src/components/ExampleArea.tsx
+++ src/components/ExampleArea.tsx
@@ -22,6 +22,10 @@ export const ExampleArea = ({ examples }: IProps) => {
 		window.open(`https://translate.google.com/saved?sl=nl&tl=en&text=${example.dutch}&op=translate`, '_blank');
 	}
 
+	const handleToggleExampleLearned = (example: DutchVerbExample) => {
+		// toggle
+	}
+
 
 	return (
 		<div className="text-sm -mt-3">
@@ -30,7 +34,7 @@ export const ExampleArea = ({ examples }: IProps) => {
 					<div key={example.dpodId} className="mt-3">
 						<div onClick={() => handleExampleToggle(example)} className="border rounded-t-md py-1 px-2 bg-slate-200 italic text-slate-800 cursor-pointer border-r-slate-400 border-l-slate-400 border-t-slate-400">{example.english}</div>
 						{example.isOpen && (
-							<div className="border rounded-b-md py-1 px-2 bg-slate-300 font-bold border-r-slate-400 border-l-slate-400 border-b-slate-400">{example.dutch}<PiSpeakerHighFill onClick={() => handleGoToAudioSite(example)} className="cursor-pointer inline-block ml-1" /><MdOutlineCheckBoxOutlineBlank onClick={() => handleGoToAudioSite(example)} className="cursor-pointer inline-block ml-1" /></div>
+							<div className="border rounded-b-md py-1 px-2 bg-slate-300 font-bold border-r-slate-400 border-l-slate-400 border-b-slate-400">{example.dutch}<PiSpeakerHighFill onClick={() => handleGoToAudioSite(example)} className="cursor-pointer inline-block ml-1" /><MdOutlineCheckBoxOutlineBlank onClick={() => handleToggleExampleLearned(example)} className="cursor-pointer inline-block ml-1" /></div>
 						)}
 					</div>
 				)
