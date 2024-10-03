bbe0a1d5045f44be17bfbaed32fb60fe39c4adc0
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-04 00:16:38 +0200

    showing checkmark

--- src/components/ExampleArea.tsx
+++ src/components/ExampleArea.tsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { AppContext } from "../AppContext";
 import { DutchVerbExample } from "../types";
 import { PiSpeakerHighFill } from "react-icons/pi";
+import { MdOutlineCheckBoxOutlineBlank } from "react-icons/md";
 
 interface IProps {
 	examples: DutchVerbExample[]
@@ -29,7 +30,7 @@ export const ExampleArea = ({ examples }: IProps) => {
 					<div key={example.dpodId} className="mt-3">
 						<div onClick={() => handleExampleToggle(example)} className="border rounded-t-md py-1 px-2 bg-slate-200 italic text-slate-800 cursor-pointer border-r-slate-400 border-l-slate-400 border-t-slate-400">{example.english}</div>
 						{example.isOpen && (
-							<div className="border rounded-b-md py-1 px-2 bg-slate-300 font-bold border-r-slate-400 border-l-slate-400 border-b-slate-400">{example.dutch}<PiSpeakerHighFill onClick={() => handleGoToAudioSite(example)} className="cursor-pointer inline-block ml-1" /></div>
+							<div className="border rounded-b-md py-1 px-2 bg-slate-300 font-bold border-r-slate-400 border-l-slate-400 border-b-slate-400">{example.dutch}<PiSpeakerHighFill onClick={() => handleGoToAudioSite(example)} className="cursor-pointer inline-block ml-1" /><MdOutlineCheckBoxOutlineBlank onClick={() => handleGoToAudioSite(example)} className="cursor-pointer inline-block ml-1" /></div>
 						)}
 					</div>
 				)
