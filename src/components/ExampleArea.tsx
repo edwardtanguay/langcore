8fb08e058d5f9e527185f6324d101976f80f8035
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-04 00:00:30 +0200

    showing audio icon

--- src/components/ExampleArea.tsx
+++ src/components/ExampleArea.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { AppContext } from "../AppContext";
-import { DutchVerbExample } from "../types"
+import { DutchVerbExample } from "../types";
+import { PiSpeakerHighFill } from "react-icons/pi";
 
 interface IProps {
 	examples: DutchVerbExample[]
@@ -24,7 +25,7 @@ export const ExampleArea = ({ examples }: IProps) => {
 					<div key={example.dpodId} className="mt-3">
 						<div onClick={() => handleExampleToggle(example)} className="border rounded-t-md py-1 px-2 bg-slate-200 italic text-slate-800 cursor-pointer border-r-slate-400 border-l-slate-400 border-t-slate-400">{example.english}</div>
 						{example.isOpen && (
-							<div className="border rounded-b-md py-1 px-2 bg-slate-300 font-bold border-r-slate-400 border-l-slate-400 border-b-slate-400">{example.dutch}</div>
+							<div className="border rounded-b-md py-1 px-2 bg-slate-300 font-bold border-r-slate-400 border-l-slate-400 border-b-slate-400">{example.dutch}<PiSpeakerHighFill className="inline-block ml-1" /></div>
 						)}
 					</div>
 				)
