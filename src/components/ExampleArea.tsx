c839e57883fb9c23afda4a664c3ee8dcc6eb86de
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-04 11:30:43 +0200

    new verbs 104

--- src/components/ExampleArea.tsx
+++ src/components/ExampleArea.tsx
@@ -43,10 +43,6 @@ export const ExampleArea = ({ examples }: IProps) => {
 				const exampleIsLearned = learnedExamples.find(m => m === example.dpodId);
 				return (
 					<div key={example.dpodId} className="mt-3">
-
-
-
-
 						<div onClick={() => handleExampleToggle(example)} className={`border rounded-t-md py-1 px-2 bg-slate-200 italic text-slate-800 cursor-pointer border-r-slate-400 border-l-slate-400 border-t-slate-400 ${exampleIsLearned ? 'bg-green-400' : ''}`}>{example.english}</div>
 						{example.isOpen && (
 							<div className={`border rounded-b-md py-1 px-2 bg-slate-300 font-bold border-r-slate-400 border-l-slate-400 border-b-slate-400`}>{example.dutch}<PiSpeakerHighFill onClick={() => handleGoToAudioSite(example)} className="cursor-pointer inline-block ml-1" />
