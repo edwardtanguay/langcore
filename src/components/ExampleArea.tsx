fe360bccabeaefd2fbe5830ebcb5e4572ee909da
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-04 12:04:47 +0200

    test red

--- src/components/ExampleArea.tsx
+++ src/components/ExampleArea.tsx
@@ -44,7 +44,7 @@ export const ExampleArea = ({ examples }: IProps) => {
 				return (
 					<div key={example.dpodId} className="mt-3">
 						[TEST: exampleIsLearned: {exampleIsLearned ? 'TRUE' : 'false'}]
-						<div onClick={() => handleExampleToggle(example)} className={`border rounded-t-md py-1 px-2 bg-slate-200 italic text-slate-800 cursor-pointer border-r-slate-400 border-l-slate-400 border-t-slate-400 ${exampleIsLearned ? 'bg-green-400' : ''}`}>{example.english}</div>
+						<div style={{backgroundColor: exampleIsLearned ? 'red' : ''}} onClick={() => handleExampleToggle(example)} className={`border rounded-t-md py-1 px-2 bg-slate-200 italic text-slate-800 cursor-pointer border-r-slate-400 border-l-slate-400 border-t-slate-400 ${exampleIsLearned ? 'bg-green-400' : ''}`}>{example.english}</div>
 						{example.isOpen && (
 							<div className={`border rounded-b-md py-1 px-2 bg-slate-300 font-bold border-r-slate-400 border-l-slate-400 border-b-slate-400`}>{example.dutch}<PiSpeakerHighFill onClick={() => handleGoToAudioSite(example)} className="cursor-pointer inline-block ml-1" />
 								{exampleIsLearned && (
