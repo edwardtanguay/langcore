d216255a0a516e420f3bfa876b4453ba2ee966b9
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-07 22:28:22 +0200

    showing one verb random

--- src/pages/PageTestDutchVerbs.tsx
+++ src/pages/PageTestDutchVerbs.tsx
@@ -6,18 +6,19 @@ export const PageTestDutchVerbs = () => {
 	const { dutchVerbs } = useContext(AppContext);
 
 	qstr.randomizeArray(dutchVerbs);
+	const dv = dutchVerbs[0]
 
 	return (
 		<>
-			<h2 className="text-xl mb-3">Test Yourself on Dutch Verbs</h2>
-			{dutchVerbs.map(dv => {
-				return (
+			{dv && (
+				<>
+					<h2 className="text-xl mb-3">Test Yourself on Dutch Verbs</h2>
 					<div key={dv.dpodId}>
 						<p>{dv.infinitive}</p>
 						<p className="text-xs">{dv.mainTextAnswer}</p>
 					</div>
-				)
-			})}
+				</>
+			)}
 		</>
 	)
 }
