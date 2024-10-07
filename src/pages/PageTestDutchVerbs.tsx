e8dd5338ee65a759e92e1d2f9723ff0797b38cdb
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-07 22:39:14 +0200

    show input

--- src/pages/PageTestDutchVerbs.tsx
+++ src/pages/PageTestDutchVerbs.tsx
@@ -6,6 +6,7 @@ export const PageTestDutchVerbs = () => {
 	const { dutchVerbs } = useContext(AppContext);
 
 	qstr.randomizeArray(dutchVerbs);
+	// const dv = dutchVerbs.find(m => m.infinitive === 'hebben')
 	const dv = dutchVerbs[0]
 
 	return (
@@ -13,10 +14,19 @@ export const PageTestDutchVerbs = () => {
 			{dv && (
 				<>
 					<h2 className="text-xl mb-3">Test Yourself on Dutch Verbs</h2>
-					<div key={dv.dpodId}>
-						<p>{dv.infinitive}</p>
-						<p className="text-xs">{dv.mainTextAnswer}</p>
-					</div>
+					<form>
+						<div key={dv.dpodId}>
+							<p className="text-orange-900 mb-2 text-xl">{dv.infinitive}</p>
+							{/* <p>{dv.mainTextAnswer}</p> */}
+						</div>
+						<div>
+							<input className="w-full text-sm" />
+							<button className="buttonNormal mt-3 w-full" type="button">check answer</button>
+						</div>
+						<div className="mt-6">
+							<p><span>e.g.</span><span className="italic"> heb hebben had hadden gehad</span></p>
+						</div>
+					</form>
 				</>
 			)}
 		</>
