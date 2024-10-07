043b2d2a894f52710133776d579fc2e44da222ba
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-07 23:36:19 +0200

    showing right/wrong

--- src/pages/PageTestDutchVerbs.tsx
+++ src/pages/PageTestDutchVerbs.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useContext, useEffect, useState } from "react";
 import { AppContext } from "../AppContext";
 import { DutchVerb } from "../types";
