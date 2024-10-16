5a281d79e1a5ec2c72433848e7311d88172409e8
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-16 15:13:53 +0200

    all ar

--- src/pages/PageSpanishVerbs.tsx
+++ src/pages/PageSpanishVerbs.tsx
@@ -10,7 +10,7 @@ export const PageSpanishVerbs = () => {
 				{spanishVerbs.map((sv) => {
 					return (
 						<div key={sv.verbBase} className="bg-slate-300 w-fit py-1 px-2 mb-3">
-							<p><span className="font-semibold">{sv.conj.base._1INFI}</span> - <span>{sv.conj.base._1PRPA}</span> - <span>{sv.conj.base._1PAPA}</span> - <span>(<a className="underline" href={sv.conjugationUrl} target="_blank">conjugate</a>)</span></p>
+							<p className="mb-2"><span className="font-semibold text-[1.1rem] data">{sv.conj.base._1INFI}</span> - <span className="data">{sv.conj.base._1PRPA}</span> - <span className="data">{sv.conj.base._1PAPA}</span> - <span>(<a className="underline" href={sv.conjugationUrl} target="_blank">conjugate</a>)</span></p>
 							<table className="spanishVerb">
 								<thead>
 									<tr>
@@ -18,14 +18,20 @@ export const PageSpanishVerbs = () => {
 										<th>yo</th>
 										<th>tú</th>
 										<th>él</th>
+										<th>nosotros</th>
+										<th>vosotros</th>
+										<th>ellos</th>
 									</tr>
 								</thead>
 								<tbody>
 									<tr>
 										<td>present</td>
-										<td>nnn</td>
-										<td>nnn</td>
-										<td>nnn</td>
+										<td className="data">{sv.conj.indicative._2PRES.yo}</td>
+										<td className="data">{sv.conj.indicative._2PRES.tu}</td>
+										<td className="data">{sv.conj.indicative._2PRES.el}</td>
+										<td className="data">{sv.conj.indicative._2PRES.nosotros}</td>
+										<td className="data">{sv.conj.indicative._2PRES.vosotros}</td>
+										<td className="data">{sv.conj.indicative._2PRES.ellos}</td>
 									</tr>
 								</tbody>
 							</table>
