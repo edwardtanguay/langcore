31805f972eeb4c61f00c2dbfd58f200b515ea50d
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-20 23:47:30 +0200

    showing phrases again

--- src/components/TenseExamplePanel.tsx
+++ src/components/TenseExamplePanel.tsx
@@ -3,6 +3,7 @@ import { SpanishPronoun, SpanishVerb, SpanishVerbTenseIdCode } from "../types";
 import { tenses } from '../spanish';
 import { spanishPronounTexts } from "../types";
 import { HtmlListVerbConjugations } from "./HtmlListVerbConjugations";
+import { useStoreState } from "../store/hooks";
 // import { spanishPronounIdCodes } from "../types";
 
 interface IProps {
@@ -15,16 +16,14 @@ interface IProps {
 
 export const TenseExamplePanel = ({ areaIdCode, sv, tenseIdCode, pronoun, pronounIndex }: IProps) => {
 	// const { appMode } = useStoreState((state) => state.profileModel);
-	// const { spanishExamples } = useStoreState((state) => state.profileModel);
+	const { spanishExamples } = useStoreState((state) => state.profileModel);
 
 	const tense = tenses[tenseIdCode];
 	const tenseClass = `tense${tenseIdCode}`;
 	const conjugationText = sv.conj.indicative[tenseIdCode][pronoun];
-	// const localSpanishExamples = spanishExamples.filter(
-	// 	(m) => m.verb === sv.spanish && m.tense === tenseIdCode
-	// );
-
-
+	const localSpanishExamples = spanishExamples.filter(
+		(m) => m.verb === sv.spanish && m.tense === tenseIdCode
+	);
 
 	return (
 		<tr className="bg-gray-300 text-[#222] font-mono text-xs">
@@ -32,9 +31,9 @@ export const TenseExamplePanel = ({ areaIdCode, sv, tenseIdCode, pronoun, pronou
 				{areaIdCode === 'main' && (
 					<div className="mb-6">
 						<h2 className="mb-3">1. <span className="font-bold">{tense.title}</span> tense regular verb endings:</h2>
-						<HtmlListVerbConjugations tense={tense} verbType="ar"/>
-						<HtmlListVerbConjugations tense={tense} verbType="er"/>
-						<HtmlListVerbConjugations tense={tense} verbType="ir"/>
+						<HtmlListVerbConjugations tense={tense} verbType="ar" />
+						<HtmlListVerbConjugations tense={tense} verbType="er" />
+						<HtmlListVerbConjugations tense={tense} verbType="ir" />
 					</div>
 				)}
 
@@ -46,6 +45,36 @@ export const TenseExamplePanel = ({ areaIdCode, sv, tenseIdCode, pronoun, pronou
 					</div>
 				)}
 
+				<ul className="list-disc ml-6">
+					{tense.rules.map((rule, index) => {
+
+						const localSpanishExamplesWithReason = localSpanishExamples.filter(
+							(m) => m.reason === rule.idCode
+						);
+
+						return (
+							<>
+								<li key={index} className="mt-3 mb-1"> <span className="font-bold text-[.9rem]">{rule.description}</span> </li>
+
+
+								<ul className="list-disc ml-6">
+									{localSpanishExamplesWithReason
+										.filter((m) => m.pronoun === pronoun || areaIdCode === "main")
+										.map((m, index) => (
+											<li key={index} className="mb-1">
+												<span className={`tense${m.tense}`}>{m.spanish}</span>
+												<ul className="list-disc ml-6">
+													<li>{m.english}</li>
+												</ul>
+											</li>
+										))}
+								</ul>
+
+							</>
+
+						)
+					})}
+				</ul>
 
 
 			</td>
