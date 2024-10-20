4e0d741c6ec0ef4d69b9439e063dbed2b253b2ba
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-21 00:09:42 +0200

    showing old text

--- src/components/AiExampleGeneration.tsx
+++ src/components/AiExampleGeneration.tsx
@@ -15,7 +15,18 @@ export const AiExampleGeneration = ({ sv, tenseIdCode, pronounIdCode, pronounTex
 	return (
 		<>
 			{appMode === 'dev' && (
-				<p>okok</p>
+				<fieldset className="ml-3 border border-gray-800 rounded bg-gray-400" style={{ padding: '.3rem .6rem .6rem .6rem' }} >
+					<legend className="bg-gray-700 px-1 py-0 text-gray-200">AI generation text</legend>
+					<input className="w-full mb-2 bg-gray-300 text-[.6rem]" value="${getAiExampleGenerationText(
+				conjugationText,
+				pronounText,
+				rule
+			)}"/>
+					<div className="flex gap-2 h-[1rem]">
+						<div style={{ whiteSpace: 'nowrap' }} className="justify-center align-middle">add to spanishExamples.spe.txt:</div>
+						<input className="bg-gray-300 w-full" readOnly value="${`${sv.spanish}; ${tenseIdCode}; ${pronounIdCode}; ${rule.idCode}; `}" />
+					</div>
+				</fieldset>
 			)}
 		</>
 	)
