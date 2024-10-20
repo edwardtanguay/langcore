e211053d7d60bebb9c373a4466e7b2dcc6509005
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-21 00:13:00 +0200

    finished: react component now

--- src/components/AiExampleGeneration.tsx
+++ src/components/AiExampleGeneration.tsx
@@ -10,6 +10,15 @@ interface IProps {
 	rule: SpanishTenseRule
 }
 
+const getAiExampleGenerationText = (
+	conjugationText: string,
+	pronounText: string,
+	rule: SpanishTenseRule
+) => {
+	return `list 3 Spanish sentences using "${conjugationText}" and "${pronounText}" for ${rule.description} with English translations, without parentheses, each Spanish/English with a period at the end and the sentences separated by a semi-colon, like this: Sentence one.;Sentence two.`;
+};
+
+
 export const AiExampleGeneration = ({ sv, tenseIdCode, pronounIdCode, pronounText, conjugationText, rule }: IProps) => {
 	const { appMode } = useStoreState((state) => state.profileModel);
 	return (
@@ -17,11 +26,11 @@ export const AiExampleGeneration = ({ sv, tenseIdCode, pronounIdCode, pronounTex
 			{appMode === 'dev' && (
 				<fieldset className="ml-3 border border-gray-800 rounded bg-gray-400" style={{ padding: '.3rem .6rem .6rem .6rem' }} >
 					<legend className="bg-gray-700 px-1 py-0 text-gray-200">AI generation text</legend>
-					<input className="w-full mb-2 bg-gray-300 text-[.6rem]" value="${getAiExampleGenerationText(
-				conjugationText,
-				pronounText,
-				rule
-			)}"/>
+					<input className="w-full mb-2 bg-gray-300 text-[.6rem]" value={getAiExampleGenerationText(
+						conjugationText,
+						pronounText,
+						rule
+					)} />
 					<div className="flex gap-2 h-[1rem]">
 						<div style={{ whiteSpace: 'nowrap' }} className="justify-center align-middle">add to spanishExamples.spe.txt:</div>
 						<input className="bg-gray-300 w-full" readOnly value={`${sv.spanish}; ${tenseIdCode}; ${pronounIdCode}; ${rule.idCode};`} />
