51cdca5b2c080471eafa2e121d6997df14a0cc99
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-20 23:56:17 +0200

    okok

--- src/components/GenericExamples.tsx
+++ src/components/GenericExamples.tsx
@@ -9,7 +9,17 @@ export const GenericExamples = ({ rule, areaIdCode }: IProps) => {
 	return (
 		<>
 			{areaIdCode === 'main' && (
-				<p>{rule.description}</p>
+				<>
+					<p>okok</p>
+					{/* {rule.examples
+						.map((example) => {
+							return `<li class="text-gray-600 italic">${example.spanish}
+					<ul class="list-disc ml-6">
+						<li>${example.english}</li>
+					</ul>
+				</li>`;
+						}) */}
+				</>
 			)}
 		</>
 	)
