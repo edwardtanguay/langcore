294ba8d3bd27cd6959444f837e9634be7263d010
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-20 20:17:56 +0200

    before sup change

--- src/components/SpanishTenseBlock.tsx
+++ src/components/SpanishTenseBlock.tsx
@@ -58,9 +58,9 @@ export const SpanishTenseBlock = ({ sv, title, tenseIdCode }: IProps) => {
 
 	return (
 		<>
-			<tr className="text-[.8rem]">
+			<tr className="text-[.8rem] verbBlock">
 				<td><span onClick={() => handleToggleShowInfos('main')} className="cursor-pointer select-none hover:underline" style={{ fontWeight: showInfos.main ? 'bold' : 'normal' }}>{title}</span></td>
-				<td className={`${tenseClass} cursor-pointer select-none hover:underline`} onClick={() => handleToggleShowInfos('yo')} style={{ fontWeight: showInfos.yo ? 'bold' : 'normal' }}>{sv.conj.indicative[tenseIdCode].yo}</td>
+				<td className={`${tenseClass} cursor-pointer select-none hover:underline`} onClick={() => handleToggleShowInfos('yo')} style={{ fontWeight: showInfos.yo ? 'bold' : 'normal' }}>{sv.conj.indicative[tenseIdCode].yo}<sup>0</sup></td>
 				<td className={`${tenseClass} cursor-pointer select-none hover:underline`} onClick={() => handleToggleShowInfos('tu')} style={{ fontWeight: showInfos.tu ? 'bold' : 'normal' }}>{sv.conj.indicative[tenseIdCode].tu}</td>
 				<td className={tenseClass} dangerouslySetInnerHTML={{ __html: buildTatoebaLinkElement(sv.conj.indicative[tenseIdCode].el) }}></td>
 				<td className={tenseClass} dangerouslySetInnerHTML={{ __html: buildTatoebaLinkElement(sv.conj.indicative[tenseIdCode].nosotros) }}></td>
