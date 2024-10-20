39245e8a8d9a04ecf7777b73be2aceba9f48c5d7
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-20 02:03:11 +0200

    listing out lines

--- src/classes/spanishExampleManager.ts
+++ src/classes/spanishExampleManager.ts
@@ -1,13 +1,27 @@
+import { SpanishExample } from "../types";
+import * as qstr from "../qtools/qstr";
 
 export class SpanishExampleManager {
 	private spanishExampleText = "";
+	private exampleLines: string[] = [];
+	private spanishExamples: SpanishExample[] = [];
 
 	constructor(spanishExampleText: string) {
-		this.spanishExampleText = spanishExampleText
-		console.log(111, this.spanishExampleText);
+		this.spanishExampleText = spanishExampleText;
+		this.exampleLines = qstr.convertStringBlockToLines(
+			this.spanishExampleText
+		);
+		this.parseExamples();
 	}
 
-	getExamples() {
+	public getExamples() {
 		return [];
+	}
+
+	private parseExamples() {
+		for (const _line of this.exampleLines) {
+			const line = _line.trim();
+			console.log(line);
+		}
 	}
 }
