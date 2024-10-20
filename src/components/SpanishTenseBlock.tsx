2a00f7af925e9b383e74e4ddfc1bb5923eaf34a2
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-20 14:24:27 +0200

    showing for main/yo

--- src/components/SpanishTenseBlock.tsx
+++ src/components/SpanishTenseBlock.tsx
@@ -4,6 +4,16 @@ import { getTenseHelp } from "../spanish";
 import { useStoreState } from "../store/hooks";
 import { buildTatoebaLinkElement } from "../spanish";
 
+const _initialShowInfos = {
+	main: false,
+	yo: false,
+	tu: false,
+	el: false,
+	nosotros: false,
+	vosotros: false,
+	ellos: false,
+}
+
 interface IProps {
 	sv: SpanishVerb;
 	title: string;
@@ -11,30 +21,57 @@ interface IProps {
 }
 
 export const SpanishTenseBlock = ({ sv, title, tenseIdCode }: IProps) => {
-	const [showInfo, setShowInfo] = useState(false);
+	const [showInfos, setShowInfos] = useState(_initialShowInfos);
 	const { appMode } = useStoreState((state) => state.profileModel);
 	const { spanishExamples } = useStoreState((state) => state.profileModel);
 
 	const tenseClass = "tense" + tenseIdCode;
 
-	const handleToggleShowInfo = () => {
-		setShowInfo(!showInfo);
+	const handleToggleShowInfos = (areaIdCode: string) => {
+		const _showInfos = structuredClone(_initialShowInfos);
+		switch (areaIdCode) {
+			case 'main':
+				if (showInfos.main) {
+					_showInfos.main = false;
+				} else {
+					_showInfos.main = true;
+				}
+				break;
+			case 'yo':
+				if (showInfos.yo) {
+					_showInfos.yo = false;
+				} else {
+					_showInfos.yo = true;
+				}
+				break;
+		}
+		console.log(111, _showInfos);
+		setShowInfos(_showInfos);
 	}
 
 	return (
 		<>
 			<tr className="text-[.8rem]">
-				<td><span onClick={handleToggleShowInfo} className="cursor-pointer select-none hover:underline">{title}</span></td>
-				<td className={tenseClass} dangerouslySetInnerHTML={{ __html: buildTatoebaLinkElement(sv.conj.indicative[tenseIdCode].yo) }}></td>
+				<td><span onClick={() => handleToggleShowInfos('main')} className="cursor-pointer select-none hover:underline" style={{ fontWeight: showInfos.main ? 'bold' : 'normal' }}>{title}</span></td>
+				<td className={`${tenseClass} cursor-pointer select-none hover:underline`} onClick={() => handleToggleShowInfos('yo')} style={{ fontWeight: showInfos.yo ? 'bold' : 'normal' }}>{sv.conj.indicative[tenseIdCode].yo}</td>
+				{/* <td className={tenseClass} dangerouslySetInnerHTML={{ __html: buildTatoebaLinkElement(sv.conj.indicative[tenseIdCode].yo) }}></td> */}
 				<td className={tenseClass} dangerouslySetInnerHTML={{ __html: buildTatoebaLinkElement(sv.conj.indicative[tenseIdCode].tu) }}></td>
 				<td className={tenseClass} dangerouslySetInnerHTML={{ __html: buildTatoebaLinkElement(sv.conj.indicative[tenseIdCode].el) }}></td>
 				<td className={tenseClass} dangerouslySetInnerHTML={{ __html: buildTatoebaLinkElement(sv.conj.indicative[tenseIdCode].nosotros) }}></td>
 				<td className={tenseClass} dangerouslySetInnerHTML={{ __html: buildTatoebaLinkElement(sv.conj.indicative[tenseIdCode].vosotros) }}></td>
 				<td className={tenseClass} dangerouslySetInnerHTML={{ __html: buildTatoebaLinkElement(sv.conj.indicative[tenseIdCode].ellos) }}></td>
 			</tr>
-			{showInfo && (
+			{showInfos.main && (
 				<tr className="bg-gray-300 text-[#222] font-mono text-xs">
 					<td colSpan={7} style={{ outline: '5px solid #aaa', borderRadius: '.5rem', padding: '1rem' }} >
+						<div dangerouslySetInnerHTML={{ __html: getTenseHelp(sv, tenseIdCode, appMode, spanishExamples) }}></div>
+					</td>
+				</tr>
+			)}
+			{showInfos.yo && (
+				<tr className="bg-gray-300 text-[#222] font-mono text-xs">
+					<td colSpan={7} style={{ outline: '5px solid #aaa', borderRadius: '.5rem', padding: '1rem' }} >
+						[YO]
 						<div dangerouslySetInnerHTML={{ __html: getTenseHelp(sv, tenseIdCode, appMode, spanishExamples) }}></div>
 					</td>
 				</tr>
