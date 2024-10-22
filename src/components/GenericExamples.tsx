8b75050c14ab771ec171470d3298486c0019a54e
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-22 09:08:26 +0200

    general examples are for all tenses

--- src/components/GenericExamples.tsx
+++ src/components/GenericExamples.tsx
@@ -5,22 +5,18 @@ interface IProps {
 	areaIdCode: string
 }
 
-export const GenericExamples = ({ rule, areaIdCode }: IProps) => {
+export const GenericExamples = ({ rule }: IProps) => {
 	return (
-		<>
-			{areaIdCode === 'main' && (
-				<ul className="list-disc ml-3">
-					{rule.examples.map((example) => {
-						return (
-							<li className="text-gray-600 italic">{example.spanish}
-								<ul className="list-disc ml-6">
-									<li>{example.english}</li>
-								</ul>
-							</li>
-						)
-					})}
-				</ul>
-			)}
-		</>
+		<ul className="list-disc ml-3">
+			{rule.examples.map((example) => {
+				return (
+					<li className="text-gray-600 italic">{example.spanish}
+						<ul className="list-disc ml-6">
+							<li>{example.english}</li>
+						</ul>
+					</li>
+				)
+			})}
+		</ul>
 	)
 }
