56231368c4df5fd23e118d07a2aacb8dd0591e15
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-22 20:38:59 +0200

    finished useContext

--- src/pages/PageDutchVerbs.tsx
+++ src/pages/PageDutchVerbs.tsx
@@ -5,21 +5,17 @@ import * as config from '../config.ts';
 export const PageDutchVerbs = () => {
 	const { dutchVerbs } = useStoreState((state) => state.profileModel);
 	const { learnedVerbs } = useStoreState((state) => state.profileModel);
-
 	const percent = ((learnedVerbs.length / dutchVerbs.length) * 100).toFixed(0);
-
 	const infinitivesWithoutExamples = dutchVerbs.filter(m => m.examples.length === 0).map(m => m.infinitive);
 
 	return (
 		<>
-			<span>count: {dutchVerbs.length}</span>
-			{/* <pre>obj: {JSON.stringify(dutchVerbs[0], null, 2)}</pre> */}
 			<h2 className="mb-4 text-center">You have learned {learnedVerbs.length} of {dutchVerbs.length} verbs: <b>{percent}%</b></h2>
 			{config.devMode() && infinitivesWithoutExamples.length > 0 && (
 				<p className="text-xs text-red-700 mb-3">There are {infinitivesWithoutExamples.length} verbs without examples: {infinitivesWithoutExamples.join(', ')}</p>
 			)}
 			{dutchVerbs.map(dutchVerb => {
-				return <SingularDutchVerb key={dutchVerb.dpodId} dutchVerb={dutchVerb} learnedVerbs={learnedVerbs}/>
+				return <SingularDutchVerb key={dutchVerb.dpodId} dutchVerb={dutchVerb} learnedVerbs={learnedVerbs} />
 			})}
 		</>
 	)
--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,23 +1,10 @@
 import { NavLink } from "react-router-dom";
-import { useStoreState } from "../store/hooks";
-import { useSelectors } from "../store/useSelectors";
 
 export const PageWelcome = () => {
-	const { dutchVerbs } = useStoreState((state) => state.profileModel);
-	const { getTestNumber, getTestMessage, getRandomNotAnsweredCorrectlyVerb } = useSelectors();
-
-	const randomeVerb = getRandomNotAnsweredCorrectlyVerb();
-
 	return (
 		<>
 			<p>Welcome to the Language Core, currently we have <NavLink className="underline" to="/dutchVerbs"
-			>Dutch Verbs</NavLink>.</p>
-
-			<p>test 001: {dutchVerbs.map(m => m.infinitive).join(', ')}</p>
-
-			<p>test 002: {getTestNumber()}</p>
-			<p>test 003: {getTestMessage()}</p>
-			<p>test 004: {randomeVerb.infinitive}</p>
+			>Dutch and Spanish Verbs</NavLink>.</p>
 		</>
 	)
 }
