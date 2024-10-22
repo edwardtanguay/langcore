07c7de9352b3dbf8c68bf67a0585a78e8ad605b7
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-22 19:41:21 +0200

    selectors with easypeasy state works

--- src/store/useSelectors.tsx
+++ src/store/useSelectors.tsx
@@ -1,9 +1,13 @@
+import { useStoreState } from "./hooks";
+
 export const useSelectors = () => {
+	const { dutchVerbs } = useStoreState((state) => state.profileModel);
+
 	const getTestNumber = () => {
 		return 999;
-	}	
+	}
 	const getTestMessage = () => {
-		return 'nnn';
+		return `There are ${dutchVerbs.length} verbs.`
 	}
 
 	return [getTestNumber, getTestMessage];
