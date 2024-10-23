405a71d75895be8e0e840d1a6a79a9079aa37825
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-23 20:37:44 +0200

    moved

--- src/classes/spanishVerbBuilder.ts
+++ src/classes/spanishVerbBuilder.ts
@@ -26,6 +26,58 @@ export class SpanishVerbBuilder {
 		}
 	}
 
+	private parse() {
+		const { verb, english, rank, rest } = this.getVerbLineParts();
+		const verbKind: VerbKind = rest === "regular" ? "regular" : "irregular";
+		const verbBase = verb.slice(0, -2); // habl
+		const verbType: SpanishVerbType = verb.slice(-2) as SpanishVerbType;
+		this._spanishVerb = {
+			spanish: verb,
+			english,
+			rank,
+			verbKind,
+			verbBase,
+			verbType,
+			conjugation1Url: `https://www.123teachme.com/spanish_verb_conjugation/${verb}`,
+			conjugation2Url: `https://conjugator.reverso.net/conjugation-spanish-verb-${verb}.html`,
+			conj: {
+				base: {
+					_1INFI: verb,
+					_1PRPA: this.create_1PRPA(verbType, verbBase),
+					_1PAPA: this.create_1PAPA(verbType, verbBase),
+				},
+				indicative: {
+					_2PRES: this.createVerbTenses(verbType, verbBase, "_2PRES"),
+					_2IMPE: this.createVerbTenses(verbType, verbBase, "_2IMPE"),
+					_2PRET: this.createVerbTenses(verbType, verbBase, "_2PRET"),
+					_2PRPE: this.createVerbTenses(verbType, verbBase, "_2PRPE"),
+					_2PAPE: this.createVerbTenses(verbType, verbBase, "_2PAPE"),
+					_2PREP: "hablarás", // Present (You speak)
+					_2FUIN: "hablaré", // Future (I will speak)
+					_2FUTU: "hablará", // Future (He/She/You formal will speak)
+					_2FUTP: "hablaremos", // Future (We will speak)
+					_2COND: "hablarían", // Conditional (They would speak)
+					_2CONP: "hablarías", // Conditional (You would speak)
+				},
+				subjunctive: {
+					_3PRES: "hable", // Present (I speak)
+					_3IMP1: "hablase", // Imperfect (I spoke)
+					_3IMP2: "hablara", // Imperfect (I spoke)
+					_3PRPE: "hablare", // Future (I will speak)
+					_3PAP1: "hable", // Present (I speak)
+					_3PAP2: "habléis", // Present (You all speak)
+					_3FUTU: "hablare", // Future (I will speak)
+					_3FUTP: "hablaran", // Future (They will speak)
+				},
+				imperative: {
+					_4AFFI: "habla", // Affirmative (Speak!)
+					_4NEGA: "no hables", // Negative (Don't speak!)
+				},
+			},
+		};
+		this.applyExceptions();
+	}
+
 	private getVerbLineParts(): VerbLineParts {
 		const parts = qstr.breakIntoParts(this._verbLine, ";");
 		return {
@@ -100,58 +152,6 @@ export class SpanishVerbBuilder {
 			this._spanishVerb.spanish =
 				this._spanishVerb.spanish + "+IRREGULAR";
 		}
-	}
-
-	private parse() {
-		const { verb, english, rank, rest } = this.getVerbLineParts();
-		const verbKind: VerbKind = rest === "regular" ? "regular" : "irregular";
-		const verbBase = verb.slice(0, -2); // habl
-		const verbType: SpanishVerbType = verb.slice(-2) as SpanishVerbType;
-		this._spanishVerb = {
-			spanish: verb,
-			english,
-			rank,
-			verbKind,
-			verbBase,
-			verbType,
-			conjugation1Url: `https://www.123teachme.com/spanish_verb_conjugation/${verb}`,
-			conjugation2Url: `https://conjugator.reverso.net/conjugation-spanish-verb-${verb}.html`,
-			conj: {
-				base: {
-					_1INFI: verb,
-					_1PRPA: this.create_1PRPA(verbType, verbBase),
-					_1PAPA: this.create_1PAPA(verbType, verbBase),
-				},
-				indicative: {
-					_2PRES: this.createVerbTenses(verbType, verbBase, "_2PRES"),
-					_2IMPE: this.createVerbTenses(verbType, verbBase, "_2IMPE"),
-					_2PRET: this.createVerbTenses(verbType, verbBase, "_2PRET"),
-					_2PRPE: this.createVerbTenses(verbType, verbBase, "_2PRPE"),
-					_2PAPE: this.createVerbTenses(verbType, verbBase, "_2PAPE"),
-					_2PREP: "hablarás", // Present (You speak)
-					_2FUIN: "hablaré", // Future (I will speak)
-					_2FUTU: "hablará", // Future (He/She/You formal will speak)
-					_2FUTP: "hablaremos", // Future (We will speak)
-					_2COND: "hablarían", // Conditional (They would speak)
-					_2CONP: "hablarías", // Conditional (You would speak)
-				},
-				subjunctive: {
-					_3PRES: "hable", // Present (I speak)
-					_3IMP1: "hablase", // Imperfect (I spoke)
-					_3IMP2: "hablara", // Imperfect (I spoke)
-					_3PRPE: "hablare", // Future (I will speak)
-					_3PAP1: "hable", // Present (I speak)
-					_3PAP2: "habléis", // Present (You all speak)
-					_3FUTU: "hablare", // Future (I will speak)
-					_3FUTP: "hablaran", // Future (They will speak)
-				},
-				imperative: {
-					_4AFFI: "habla", // Affirmative (Speak!)
-					_4NEGA: "no hables", // Negative (Don't speak!)
-				},
-			},
-		};
-		this.applyExceptions();
 	}
 
 	public getVerb(): SpanishVerb {
